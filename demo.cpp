8ae4d89349572b5e28fbd5877880eeac9700528d
Navin Avadhani <knavinavadhani@gmail.com>
2024-07-19 17:53:41 +0530

    Completed until pattern08

--- demo.cpp
+++ demo.cpp
@@ -33,9 +33,54 @@ void pattern04(int n) {
     cout << endl;
   }
 }
+void pattern05(int n) {
+  for (int i = n; i > 0; i--) {
+    for (int j = 1; j <= i; j++) {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+void pattern07(int n) {
+  for (int i = 0; i < n; i++) {
+  // spaces = n-i-1
+  for (int j = 0; j < n-i-1; j++)
+        {
+            cout <<" ";
+        }
+  // stars = 2i+1
+  for(int j = 0; j < 2*i+1; j++){
+            
+            cout<<"*";
+        }
+  // spaces = n-i-1
+  for (int j = 0; j < n-i-1; j++)
+        {
+            cout <<" ";
+        }
+  cout << endl;
+  }
+}
+void pattern08(int n) {
+  for (int i = 0; i < n; i++) {
+    // spaces = i
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    // stars = 2*(n-i)-1
+    for (int j = 0; j < 2*(n-i)-1; j++) {
+      cout << "*";
+    }
+    // spaces = i
+    for (int j = 0; j < i; j++) {
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
 
 int main() {
   int n;
   cin >> n;
-  pattern04(n);
+  pattern08(n);
 }
